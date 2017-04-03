2f95a75a562aa67d4cce4141d36b8f2037a6e34e
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2017-04-03 15:59:51 -0500

    chore: Troublehsooting fs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('./build'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
