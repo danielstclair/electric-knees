3de4e651a0e83bd3282f189be53ef24257acb6dc
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2017-04-03 15:16:31 -0500

    chore: Adding initial files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import App from './components/App';
+import reducers from './reducers';
 import './styles/index.css';
 
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
