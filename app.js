12044b23b51053cf81d89b649403c731fbdd6bbf
DANIEL A. ST CLAIR <danielstclair@gmail.com>
2017-04-03 20:55:14 -0500

    chore: Troubleshooting files.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./build'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.send(path.join(__dirname, './build', 'index.html'));
 });
 
 const port = process.env.PORT || 9000
